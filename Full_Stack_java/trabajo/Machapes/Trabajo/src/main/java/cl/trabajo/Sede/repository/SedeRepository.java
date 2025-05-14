bff851c56c7c81522edebb1f939e44f637b20d4d
unknown <DUOC@SBAWSD03LC229.duoc.acad>
2025-05-14 13:42:58 -0400

    tabla sede removida

--- Full_Stack_java/trabajo/Machapes/Trabajo/src/main/java/cl/trabajo/Sede/repository/SedeRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.trabajo.Sede.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import cl.trabajo.Sede.dto.SedeDTO;
-
-public interface SedeRepository extends CrudRepository<SedeDTO, Integer> {
-
-}
