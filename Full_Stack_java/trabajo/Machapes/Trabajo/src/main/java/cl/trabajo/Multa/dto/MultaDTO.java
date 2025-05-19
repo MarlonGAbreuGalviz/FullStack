e36a1afa005226a9279b056de2d1120f4b1c3a3a
unknown <DUOC@SBAWSD03LC125.duoc.acad>
2025-05-19 15:16:06 -0400

    guardado error multa

--- Full_Stack_java/trabajo/Machapes/Trabajo/src/main/java/cl/trabajo/Multa/dto/MultaDTO.java
+++ Full_Stack_java/trabajo/Machapes/Trabajo/src/main/java/cl/trabajo/Multa/dto/MultaDTO.java
@@ -1,11 +1,11 @@
 package cl.trabajo.Multa.dto;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity; 
-import jakarta.persistence.Id; 
-import jakarta.persistence.Table; 
-import lombok.AllArgsConstructor; 
-import lombok.Getter; 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor; 
 import lombok.Setter;
 
